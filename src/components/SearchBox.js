c06a47de56d4e7c0476bf7401c7ea1e85bf13953
Sai Krishna Reddy Katta <saikrishnareddyk95@gmail.com>
2021-09-20 01:24:33 -0700

    added debouncing functionality

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,20 +1,46 @@
+import { useCallback } from "react";
+
 function SearchBox(props) {
-  function changeHandler(value) {
+  //   function changeHandler(value) {
+  //     props.setSearchValue(value);
+  //   }
+
+  const debounce = (fn, delay) => {
+    let timeoutId;
+    return function (...args) {
+      if (timeoutId) {
+        clearTimeout(timeoutId);
+      }
+      timeoutId = setTimeout(() => {
+        fn(...args);
+      }, delay);
+    };
+  };
+  //Using Callback
+  //   const deb = useCallback(
+  //     debounce((value) => props.setSearchValue(value), 1000),
+  //     []
+  //   );
+
+  //   const betterFunction = (value) => {
+  //     deb(value);
+  //   };
+
+  const betterFunction = debounce((value) => {
     props.setSearchValue(value);
-  }
+  }, 1000);
+
   return (
     <div className="col col-sm-3">
       <input
-        value={props.searchValue}
+        // value={props.searchValue}
         type="text"
         className="form-control"
         placeholder="search for movie"
-        onChange={(event) => changeHandler(event.target.value)}
+        onChange={(event) => betterFunction(event.target.value)}
       />
     </div>
   );
 }
 
 export default SearchBox;
-
-// onChange={(event) => props.setSearchValue(event.target.value)}
